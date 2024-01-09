1d39bb412bd7355f0606a63a000b7e162a9ba0f9
Eder Henrique <ederhscc@gmail.com>
2024-01-09 15:25:46 -0300

    converting text in markdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, useRef } from "react";
+import { marked } from "marked";
+import "./index.css";
+import Toolbar from "./components/Toolbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("# Olá, eu sou feito de markdown!");
+  const textAreaRef = useRef(null);
+
+  // converte o texto em html
+  const rendeText = () => {
+    return { __html: marked(text)}
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app-container">
+      <Toolbar />
+      <textarea
+        ref={textAreaRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <div dangerouslySetInnerHTML={rendeText()} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Toolbar = () => {
+  return (
+    <div className="toolbar">
+      <button>h1</button>
+      <button>h2</button>
+      <button>Bold</button>
+      <button>Italic</button>
+      <button>Link</button>
+      <button>Code Block</button>
+      <button>List Item</button>
+      <button>Horizontal Line</button>
+    </div>
+  );
+};
+
+export default Toolbar;
